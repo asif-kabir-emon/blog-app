da7073c6f0286d68faf3b4de05ddf5150979cde1
MD ASIF KABIR EMON <79392749+asif-kabir-emon@users.noreply.github.com>
2024-06-08 16:00:27 +0600

    Add my_posts page

--- blog_project/posts/urls.py
+++ blog_project/posts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import add_post, edit_post, delete_post
+from .views import add_post, edit_post, delete_post, my_posts
 
 urlpatterns = [
     path("add/", add_post, name="add_post"),
     path("edit/<int:id>", edit_post, name="edit_post"),
     path("delete/<int:id>", delete_post, name="delete_post"),
+    path("my_posts/", my_posts, name="my_posts"),
 ]
--- blog_project/posts/views.py
+++ blog_project/posts/views.py
@@ -1,32 +1,44 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from .forms import PostForm
 from .models import Post
 
+@login_required
 def add_post(request):
     if request.method == 'POST':
         form = PostForm(request.POST)
         if form.is_valid():
+            form.instance.author = request.user
             form.save()
+            return redirect('my_posts')
     else:
         form = PostForm()
     return render(request, 'add_post.html', {'form': form})
 
+@login_required
 def edit_post(request, id):
     post = Post.objects.get(pk=id)
     if request.method == 'POST':
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
+            form.instance.author = request.user
             form.save()
-            return redirect('home')
+            return redirect('my_posts')
     else:
         form = PostForm(instance=post)
     return render(request, 'edit_post.html', {'form': form})
 
+@login_required
 def delete_post(request, id):
     post = Post.objects.get(pk=id)
     post.delete()
-    return redirect('home')
+    return redirect('my_posts')
+
+@login_required
+def my_posts(request):
+    posts = Post.objects.filter(author=request.user)
+    return render(request, 'my_posts.html', {'data': posts})
     
     
 
